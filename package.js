eaa87785b7a5cc0c82a05f2ff2ca790b36705693
Jeremie Parker <jparker@guimiesoft.com>
2014-09-08 17:37:39 +0200

    Update for Meteor 0.9.x new package system

--- package.js
+++ package.js
@@ -1,15 +1,18 @@
 Package.describe({
-  summary: "SHA256 algo for CryptoJS, standard secure crypto algorithms"
+	summary: 'SHA256 algo for CryptoJS, standard secure crypto algorithms',
+	version: '3.1.2-1',
+	git: 'https://github.com/p-j/meteor-crypto-sha256'
 });
 
 Package.on_use(function (api) {
-  api.use('crypto-base',    ['client', 'server']);
-  api.imply('crypto-base',  ['client', 'server']);
-  api.add_files(['sha256.js'], ['client', 'server']);
+	api.versionsFrom('METEOR@0.9.0');
+	api.use('jparker:meteor-crypto-base@3.1.2-1', ['client', 'server']);
+	api.imply('jparker:meteor-crypto-base', ['client', 'server']);
+	api.add_files(['sha256.js'], ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use(['crypto-base', 'crypto-sha256', 'tinytest']);
+	api.use(['jparker:meteor-crypto-base', 'jparker:meteor-crypto-sha256', 'tinytest']);
 
-  api.add_files('crypto-sha256_tests.js', ['client', 'server']);
+	api.add_files('crypto-sha256_tests.js', ['client', 'server']);
 });
