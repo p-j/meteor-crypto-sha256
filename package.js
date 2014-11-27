1b84799a170c31b91c2788daa7599eca59caa28c
Jeremie Parker <jparker@guimiesoft.com>
2014-11-27 19:23:46 +0100

    Added `jparker:crypto-hmac` package to the test dependencies

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
 	summary: 'SHA256 algo for CryptoJS, standard secure crypto algorithms',
-	version: '0.1.0',
+	version: '0.1.1',
 	git: 'https://github.com/p-j/meteor-crypto-sha256.git'
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
 	api.versionsFrom('METEOR@0.9.1.1');
 
 	api.use('jparker:crypto-core@0.1.0', ['client', 'server']);
@@ -14,12 +14,13 @@ Package.onUse(function (api) {
 	api.addFiles('lib/sha256.js', ['client', 'server']);
 });
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
 	api.use([
-    'jparker:crypto-core@0.1.0',
-    'jparker:crypto-sha256@0.1.0',
-    'tinytest'
-  ]);
+		'jparker:crypto-core@0.1.0',
+		'jparker:crypto-sha256@0.1.0',
+		'jparker:crypto-hmac@0.1.0',
+		'tinytest'
+	]);
 
 	api.addFiles('tests/tests.js', ['client', 'server']);
 });
